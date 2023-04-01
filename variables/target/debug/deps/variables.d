cc0e8fb167840ff2420fd694d1cd600106231095
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2023-04-01 13:38:40 -0700

    New Exercise: variables

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\3. Coding Projects\2. Learning Rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\3. Coding Projects\2. Learning Rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\3.\ Coding\ Projects\2.\ Learning\ Rust\variables\target\debug\variables.exe: C:\3.\ Coding\ Projects\2.\ Learning\ Rust\variables\src\main.rs
