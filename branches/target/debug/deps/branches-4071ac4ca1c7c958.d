6ab4b2e419a5b3835e0d977f5226b11ef1c88e6e
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2023-05-21 16:11:16 -0700

    Adding new exercises

---
+++ branches/target/debug/deps/branches-4071ac4ca1c7c958.d
@@ -0,0 +1,5 @@
+C:\3. Coding Projects\2. Learning Rust\branches\target\debug\deps\branches-4071ac4ca1c7c958.rmeta: src\main.rs
+
+C:\3. Coding Projects\2. Learning Rust\branches\target\debug\deps\branches-4071ac4ca1c7c958.d: src\main.rs
+
+src\main.rs:
---
+++ branches/target/debug/deps/branches-e3b2db0262319f49.d
@@ -0,0 +1,5 @@
+C:\3. Coding Projects\2. Learning Rust\branches\target\debug\deps\branches-e3b2db0262319f49.rmeta: src\main.rs
+
+C:\3. Coding Projects\2. Learning Rust\branches\target\debug\deps\branches-e3b2db0262319f49.d: src\main.rs
+
+src\main.rs:
